6abc6e258fbc185109344bc0d7b9bec5238148fc
Michael Allen <93445521+shokoladber@users.noreply.github.com>
2023-07-19 14:10:48 -0500

    Profile stuff. Image. CSS. Names. Fun facts. Moved button.

--- part2/exercises-2/src/components/Profile.js
+++ part2/exercises-2/src/components/Profile.js
@@ -1,13 +1,32 @@
 import './styles.css';
 import Button from './Button';
+import oceans from './oceans.json';
+
+
+const listItem = oceans.map(ocean =>
+   <div key={ocean.id} className={`${ocean.fishCheck === "true" ? "isAFish" : "profile"}`}>
+      <h1>{ocean.name}</h1>
+      <h2>Fun Facts</h2>
+      <ul>
+         <li>{ocean.fact1}</li>
+         <li>{ocean.fact2}</li>
+         <li>{ocean.fact3}</li>
+      </ul>
+      <Button />
+
+      <img src={ocean.image} alt={ocean.name} className="img"/>
+   </div>
+);
+
 
 
 function Profile()
 {
    return(
       <>
-         <h3>Coming Soon! Profiles of Ocean creatures</h3>
-         <Button />
+         <ul>
+            <li>{listItem}</li>
+         </ul>
       </>  
    );
 }
